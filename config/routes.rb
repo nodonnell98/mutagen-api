2247b52b32fd5bed188792dc92c79142b54ebc91
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-07-22 21:21:55 +0100

    weapon routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :weapons, only: [:index]
+  resources :weapons, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/weapons", to: "weapons#index"
+  get "/weapons/:id", to: "weapons#show"
+  post "/weapons", to: "weapons#create"
+  put "weapons/:id", to: "weapons#update"
+  delete "weapons/:id", to: "weapons#destroy"
 end
