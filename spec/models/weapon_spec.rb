1767ae24e7cf0dfa35a3ef5a35dc06a465a0c1cc
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-07-22 17:00:22 +0100

    weapon model covered in rspec

---
+++ spec/models/weapon_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Weapon, :type => :model do
+  let!(:weapon) { FactoryBot.build(:weapon) }
+
+  it "is valid with valid attributes" do
+    expect(weapon).to have(0).errors_on(:name)
+    expect(weapon).to be_valid
+  end
+
+  it "is not valid without a valid name" do
+    weapon.name = nil
+
+    expect(weapon).to have(1).errors_on(:name)
+    expect(weapon).to_not be_valid
+  end
+end
