542a86c437d2442a83688e778c3fac341b5c92b8
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-07-23 09:27:38 +0100

    additional weapon request specs added

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Weapons", type: :request do
   end
 
   describe 'POST /create' do
-    it 'valid bookmark attributes' do
+    it 'is successful with valid params' do
       # send a POST request to /weapon, with these parameters
       # The controller will treat them as JSON
       post '/weapons', params: {
@@ -34,14 +34,77 @@ RSpec.describe "Weapons", type: :request do
       expect(Weapon.count).to eq(2)
       expect(Weapon.last.name).to eq('Widowmaker')
     end
+
+    it 'is unsuccessful with invalid params' do
+      # send a POST request to /weapon, with these parameters
+      # The controller will treat them as JSON
+      post '/weapons', params: {
+        weapon: {
+          name: 24,
+          weapon_type: nil,
+          description: "A deadly sniper",
+          range: 200,
+          quality: "rare",
+          dice_type: 4,
+          dice_qty: nil,
+          proficiency: 2,
+          ammo: 3,
+          damage_type: "kinetic"
+        }
+      }
+
+      expect(response.status).to eq(422)
+      expect(Weapon.count).to eq(1)
+      expect(Weapon.last.name).to_not eq('Widowmaker')
+    end
   end
 
-  describe "" do
+  describe "PUT /update" do
+    it 'successfully edits weapon with valid changes' do
+      weapon_url = '/weapons/' + weapon.id.to_s
+
+      expect(Weapon.count).to eq(1)
+      expect(Weapon.last.name).to eq('Gun')
+
+      put weapon_url,  params: {
+        weapon: {
+          name: "Edited Gun"
+        }
+      }
+
+      expect(response.status).to eq(200)
+      expect(Weapon.count).to eq(1)
+      expect(Weapon.last.name).to eq('Edited Gun')
+    end
+
+    it 'will not edit weapon with invalid changes' do
+      weapon_url = '/weapons/' + weapon.id.to_s
+
+      expect(Weapon.count).to eq(1)
+      expect(Weapon.last.name).to eq('Gun')
+
+      put weapon_url,  params: {
+        weapon: {
+          name: nil
+        }
+      }
+
+      expect(response.status).to eq(422)
+      expect(Weapon.last.name).to eq('Gun')
+    end
   end
 
-  describe "" do
-  end
+  describe "DELETE /destroy" do
+    let!(:weapon2) { FactoryBot.create(:weapon, name: 'DeleteMe')}
+    it 'deletes a present record' do
+      expect(Weapon.last.name).to eq('DeleteMe')
+      expect(Weapon.count).to eq(2)
 
-  describe "" do
+      weapon_url = '/weapons/' + weapon2.id.to_s
+      delete weapon_url, as: :json
+
+      expect(response.status).to eq(204)
+      expect(Weapon.count).to eq(1)
+    end
   end
 end
