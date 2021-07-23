f3c67c29ea340541527cb9b1ad82f9c9552da3e0
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-07-23 08:44:45 +0100

    created weapon POST request spec

--- spec/requests/weapons_spec.rb
+++ spec/requests/weapons_spec.rb
@@ -10,4 +10,38 @@ RSpec.describe "Weapons", type: :request do
       expect(response).to be_successful
     end
   end
+
+  describe 'POST /create' do
+    it 'valid bookmark attributes' do
+      # send a POST request to /weapon, with these parameters
+      # The controller will treat them as JSON
+      post '/weapons', params: {
+        weapon: {
+          name: "Widowmaker",
+          weapon_type: "Sniper",
+          description: "A deadly sniper",
+          range: 200,
+          quality: "rare",
+          dice_type: 4,
+          dice_qty: 2,
+          proficiency: 2,
+          ammo: 3,
+          damage_type: "kinetic"
+        }
+      }
+
+      expect(response.status).to eq(201)
+      expect(Weapon.count).to eq(2)
+      expect(Weapon.last.name).to eq('Widowmaker')
+    end
+  end
+
+  describe "" do
+  end
+
+  describe "" do
+  end
+
+  describe "" do
+  end
 end
