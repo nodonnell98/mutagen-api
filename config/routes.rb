d42e0dab324b5cecdc03b46d8bd1c65f03b87a85
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-07-28 16:57:31 +0100

    added actions for character routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :characters
+      resources :characters, only: %i[index show create update destroy post]
       resources :weapons, only: %i[index show create update destroy post]
       resources :users, only: %i[show update]
     end
