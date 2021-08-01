6a28032eeb259971f470f016cebe7e692c1935c9
Niall O'Donnell <niall.odonnell@students.plymouth.ac.uk>
2021-08-01 10:44:36 +0100

    fix char  deletion when they have character weapon

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@
 
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :character_weapons
+  has_many :character_weapons, dependent: :destroy
   has_many :weapons, through: :character_weapons
 
   validates :name, presence: true
